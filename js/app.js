63aebb8711a4bff1f327c76c6514b3ea66b6ea56
Anupkumar-Gopi <anupgopi.webdev@gmail.com>
2016-12-02 15:01:23 +0530

    Assignment-5.2

---
+++ js/app.js
@@ -0,0 +1,9 @@
+var person = {
+	name: "Anupkumar Gopi",
+	age: 18,
+	dob: "01/01/1998",
+	pob: "Kerala"
+};
+
+console.log("Name : "+person.name+"\nAge : "+person.age+"\nDate of Birth : "+person.dob+"\nPlace of Birth : "+person.pob);
+
